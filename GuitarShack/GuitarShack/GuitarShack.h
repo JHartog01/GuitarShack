fa358c9889c88b28859858100506d3dd79b63e9e
Joshua Hartog <jhartog2@ford.com>
2021-11-18 14:48:45 +0000

    Initial Commit

---
+++ GuitarShack/GuitarShack/GuitarShack.h
@@ -0,0 +1,18 @@
+//
+//  GuitarShack.h
+//  GuitarShack
+//
+//  Created by Hartog, Joshua (J.A.) on 18/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GuitarShack.
+FOUNDATION_EXPORT double GuitarShackVersionNumber;
+
+//! Project version string for GuitarShack.
+FOUNDATION_EXPORT const unsigned char GuitarShackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GuitarShack/PublicHeader.h>
+
+
---
+++ GuitarShack/GuitarShackTests/GuitarShackTests.swift
@@ -0,0 +1,19 @@
+//
+//  GuitarShackTests.swift
+//  GuitarShackTests
+//
+//  Created by Hartog, Joshua (J.A.) on 18/11/2021.
+//
+
+import XCTest
+@testable import GuitarShack
+
+class GuitarShackTests: XCTestCase {
+
+    func testFail() {
+        XCTAssertFalse(1+1==2)
+    }
+
+}
+
+
