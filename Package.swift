43f79192661becb741e48ecbbc12754a01d249f9
Joshua Hartog <jhartog2@ford.com>
2021-11-18 15:14:42 +0000

    Updated Swift Tools

--- Package.swift
+++ Package.swift
@@ -1,10 +1,9 @@
-
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    swift-tools-version: 5.3.0,
     name: "Guitar Shack",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
